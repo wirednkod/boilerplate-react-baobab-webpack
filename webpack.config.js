b2850286589395b17f8590f333f26df681ab700b
KONTAKIS Nikolaos <nkontaki@intrasoft-intl.com>
2017-02-27 22:25:37 +0200

    ,.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-var */
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:5000',
+    'webpack/hot/dev-server',
+    './src/index'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
+};
